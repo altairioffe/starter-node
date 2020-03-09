1150d92f98b55e166310f2a6d0e82b3f9c5e78f8
Altair Ioffe <59181161+altairioffe@users.noreply.github.com>
2020-03-09 17:32:19 -0400

    pushed phone numbers to .env

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ require('dotenv').config(); //to pull .env
 // Load configuration information from system environment variables.
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
     TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
-    TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+    TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER,
+    CUSTOMER_PHONE_NUMBER= process.env.CUSTOMER_PHONE_NUMBER,
+    RESTAURANT_PHONE_NUMBER = process.env.RESTAURANT_PHONE_NUMBER;
+;
 
 // Create an authenticated client to access the Twilio REST API
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
@@ -71,10 +74,11 @@ app.get('/confirmed', function(req, res, next) {
 
 //Helper FUNCTION, send text reminding customer to leave CAN BE TEXT OR CALL
 const remindCustomerToLeave = function(timeUntilCooked, travelTime) {
+  
   let reminderTime = timeUntilCooked - travelTime;
 
   client.messages.create({
-    to: '+12266788585', //CHANGE TO ADD 2nd PHONE NUMBER;
+    to: '+12266788585', 
     from: TWILIO_PHONE_NUMBER,
     body: `You should leave to pick up your order in ${reminderTime} minutes!`
   }).then((message)=> console.log('FROM REMINDER .THEN FUNCTION: ', message.body));
@@ -82,10 +86,12 @@ const remindCustomerToLeave = function(timeUntilCooked, travelTime) {
 
 
 
-//LISTEN FOR SMS  **NEED TO CONFIGURE ROUTE ON VALID DOMAIN / SERVER / DOES NOT WORK ON LOCALHOST
+//LISTEN FOR SMS  **DO NOT CHANGE THIS ROUTE NAME** it is configured as the webhook in Twilio account
 app.post('/inbound', (req, res) => {
-  const timeUntilCooked = req.body.Body
-  const travelTime = 5
+
+  const timeUntilCooked = req.body.Body; //extraced from restaurant's SMS reply containing ETA in minutes
+  const travelTime = 5; //hard-coded time to get to the restaurant, in minutes
+
   remindCustomerToLeave(timeUntilCooked, travelTime);
 
   const twiml = new MessagingResponse();
