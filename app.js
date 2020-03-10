d70ca390fa1aea94bdd23f290284fea83cf8614f
Altair <altair.ioffe@gmail.com>
2020-03-10 17:00:59 +0000

    reflecting changes pushed to heroku repo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const twilio = require('twilio');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 require('dotenv').config(); //to pull .env 
 
-//nodemodules/twilio/lib/rest/Twilio.js
+// nodemodules/twilio/lib/rest/Twilio.js
 
 // Load configuration information from system environment variables.
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
@@ -15,7 +15,7 @@ const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
     TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER,
     CUSTOMER_PHONE_NUMBER= process.env.CUSTOMER_PHONE_NUMBER,
     RESTAURANT_PHONE_NUMBER = process.env.RESTAURANT_PHONE_NUMBER;
-;
+
 
 // Create an authenticated client to access the Twilio REST API
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
@@ -44,7 +44,7 @@ app.post('/message', function(req, res, next) {
   client.messages.create({
     to: RESTAURANT_PHONE_NUMBER,
     from: TWILIO_PHONE_NUMBER,
-    body: `New order: ${req.body.message}. ***Please respond with ETA as numerical value in MINUTES.`
+    body: `New order: ${req.body.message}.$0a***Please respond with ETA as numerical value in MINUTES.` // "%0a" encodes line break
     //'New Order Text Sent!'
   }).then(function(message) {
     // When we get a response from Twilio, respond to the HTTP POST request
@@ -71,9 +71,8 @@ app.get('/confirmed', function(req, res, next) {
 });
 
 
-
-//Helper FUNCTION, send text reminding customer to leave CAN BE TEXT OR CALL
-const confirmCustomerETA = function(reminderTime) {
+//Helper Function to send text reminding customer to leave CAN BE TEXT OR CALL
+const consfirmTimeUntilDeparture = function(reminderTime) {
   
   client.messages.create({
     to: CUSTOMER_PHONE_NUMBER, 
@@ -82,50 +81,54 @@ const confirmCustomerETA = function(reminderTime) {
   }).then((message)=> console.log('from customer ETA .THEN ', message.body));
 };
 
-// Send reminder SMS for customer to leave when timer is up:
+
+// Helper Function to send reminder SMS for customer to leave when timer is up:
 const timedReminderToLeave = function(reminderTime) {
+  console.log(`timer started for ${reminderTime} minutes`);
+
   setTimeout(() => {
-
-    console.log(`timer started for ${reminderTime} minutes`);
-
     client.messages.create({
       to: CUSTOMER_PHONE_NUMBER, 
       from: TWILIO_PHONE_NUMBER,
       body: `You should leave now to pick up your order!`
     }).then((message)=> console.log('REMINDER TO LEAVE SENT: ', message.body));
-
-  }, reminderTime * 60000);
-
+  }, reminderTime * 60000); // multiplies time until departure in minutes by 60000 to convert to milliseconds
 };
 
 
 //LISTEN FOR SMS  **DO NOT CHANGE THIS ROUTE NAME** it is configured as the webhook in Twilio account
 app.post('/inbound', (req, res) => {
 
-  const timeUntilCooked = req.body.Body; //extraced from restaurant's SMS reply containing ETA in minutes
-  const travelTime = 5; //hard-coded time to get to the restaurant, in minutes
-  let reminderTime = timeUntilCooked - travelTime;
+  const timeUntilReady = req.body.Body; //extract ETA in minutes from restaurant's SMS containing ETA confirmation
+  const travelTime = 5; //(hard-coded) travel time to get to the restaurant, in minutes
+  const reminderTime = timeUntilReady - travelTime;
 
-  confirmCustomerETA(reminderTime);
-  timedReminderToLeave(reminderTime)
+  consfirmTimeUntilDeparture(reminderTime); //send SMS to tbe customer when they should plan to leave once restaurant confirms order
+  timedReminderToLeave(reminderTime);  //starts timer that sends SMS to the customer when it's time to leave
 
   const twiml = new MessagingResponse();
-
   twiml.message('Thanks for confirming ETA!');
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  console.log('TRIGGERED FROM INBOUND ROUTE!!: ', req.body.Body);
+  res.writeHead(200, {'Content-Type': 'text/xml'}); //Response to HTTP POST request for restaurant confirming ETA, sends SMS back to restaurant
 
-  res.end(twiml.toString());
+  console.log('TRIGGERED FROM INBOUND ROUTE!!: ', req.body.Body); //debugging
+
+  res.end(twiml.toString()); //end response process
 });
 
+
+
+
+
+
+
 // Create a TwiML document to provide instructions for an outbound call
-app.post('/hello', function(req, res, next) {
+app.post('/voice', function(req, res, next) {
   // Create a TwiML generator
   const twiml = new twilio.twiml.VoiceResponse();
   // const twiml = new twilio.TwimlResponse();
-  twiml.say('Hello there! You have successfully configured a web hook.');
-  twiml.say('Good luck on your Twilio quest!', { 
-      voice:'woman' 
+  twiml.say('Do you like green eggs and ham?');
+  twiml.say('DO YOU LIKE THEM SAM I AM!', { 
+      voice:'alice' 
   });
 
   // Return an XML response to this request
