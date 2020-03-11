63cbadc0b3f40f2a532bdd9fbdeb291b9208ca72
Altair <altair.ioffe@gmail.com>
2020-03-11 01:51:32 +0000

    removed mp3 url in voice

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ const timedVoiceReminderToLeave = function(reminderTime) {
 
   client.calls
   .create({
-     twiml: '<Response><Say><You should leave now to pick up your order!</Say><Play>http://demo.twilio.com/docs/classic.mp3</Play></Response>',
+     twiml: '<Response><Say><You should leave now to pick up your order!</Say></Response>',
      to: CUSTOMER_PHONE_NUMBER,
      from: TWILIO_PHONE_NUMBER
   }).then(call => console.log('REMINDER CALL TO LEAVE SENT: ',call.sid))
